901b0ff172b16bd176de50919b1847044de388fd
ayushhub-hub <ayushsable87@gmail.com>
2025-02-18 10:00:30 +0530

    done

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Your Name</h1>
+      <p>Email: ayushsable96@gmail.com</p>
+      <p>Phone: +1234567890</p>
+    </div>
+  );
+};
+
+export default function App() {
+  return <Home />;
+}
